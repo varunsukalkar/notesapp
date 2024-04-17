071aaa44861683e4c98a3d599ab7e2954e2e44d1
varunsukalkar31 <sukalkarvarun@gmail.com>
2024-04-17 18:15:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/MainActivity2.java
@@ -0,0 +1,40 @@
+package com.example.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        // on below line we are calling handler to run a task
+        // for specific time interval
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // on below line we are
+                // creating a new intent
+                Intent i = new Intent(MainActivity2.this, MainActivity.class);
+
+                // on below line we are
+                // starting a new activity.
+                startActivity(i);
+
+                // on the below line we are finishing
+                // our current activity.
+                finish();
+            }
+        }, 5000);
+    }
+}
